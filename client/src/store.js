335bc358a5603f10f189ddaf99cf49c21393ee10
Wilmer Rodriguez <driverco@gmail.com>
2019-05-13 18:52:17 -0500

    se elimina Redux ya que no fue necesario su uso

--- client/src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, compose } from 'redux';
-import MainReducer from './reducers/MainReducer';
-//export default createStore(MainReducer);
-
-export default compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
-//const store = createStore(MainReducer, window.STATE_FROM_SERVER);
